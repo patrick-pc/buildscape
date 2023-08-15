b1ca4a655650a6cd2fc2b11d8b4ae63d76ddce83
spidΞy <web3slinger@gmail.com>
2023-08-14 20:38:04 -0500

    fix: server url env variable

--- client/src/components/SocketManager.jsx
+++ client/src/components/SocketManager.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { useEffect } from "react";
 
 export const socket = io(
-  import.meta.env.VITE_API_KEY || "http://localhost:3001"
+  import.meta.env.VITE_SERVER_URL || "http://localhost:3001"
 );
 export const charactersAtom = atom([]);
 export const userAtom = atom(null);
