1c2f80fe3b633ef8d2749cab019a4240be509880
spidΞy <web3slinger@gmail.com>
2023-08-16 14:40:29 -0500

    fix: random hex color bug

--- server/index.js
+++ server/index.js
@@ -19,7 +19,12 @@ const generateRandomPosition = () => {
 };
 
 const generateRandomHexColor = () => {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return (
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+  );
 };
 
 const players = {};
